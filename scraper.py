c8e38a4e3ad81e2aaef600de1babfe6b3797173b
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-14 12:09:22 -0800

    added parse functions for metadata and inspection information. main function reutrns a compiled dict of information.

--- scraper.py
+++ scraper.py
@@ -28,6 +28,17 @@ INSPECTION_PARAMS = {
     "Sort": "B",
 }
 
+query = {
+    "Business_Name": "Tango",
+    "Business_Address": "1100%20Pike%20St",
+    "City": "Seattle",
+    "Zip_Code": "98101"
+}
+
+UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+
+HEADERS = {'User-Agent': UA}
+
 
 def get_inspection_page(**kwargs):
     """Return inspection information for given search."""
@@ -36,7 +47,7 @@ def get_inspection_page(**kwargs):
     for key, val in kwargs.items():
         if key in INSPECTION_PARAMS:
             params[key] = val
-    resp = requests.get(url, params=params)
+    resp = requests.get(url, params=params, headers=HEADERS, stream=True)
     resp.raise_for_status()
     return resp.content, resp.encoding
 
@@ -75,11 +86,72 @@ def has_two_tds(element):
     return is_tr and has_two
 
 
+def clean_data(td):
+    """Clean data removing extra whitespace."""
+    data = td.string
+    try:
+        return data.strip(" \n:-")
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(elem):
+    """Take in a restaurant and return dict containing metadata."""
+    metadata_rows = elem.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(elem):
+    """Get inspections data from table rows."""
+    is_tr = elem.name == 'tr'
+    if not is_tr:
+        return False
+    td_children = elem.find_all('td', recursive=False)
+    has_four = len(td_children) == 4
+    this_text = clean_data(td_children[0]).lower()
+    contains_word = 'inspection' in this_text
+    does_not_start = not this_text.startswith('inspection')
+    return is_tr and has_four and contains_word and does_not_start
+
+
+def extract_score_data(elem):
+    """Extract the score for each inspection."""
+    inspection_rows = elem.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total/float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
 if __name__ == '__main__':
     kwargs = {
         'Inspection_Start': '2/1/2013',
-        'Inspection_End': '2/1/2015',
-        'Zip_Code': '98109'
+        'Inspection_End': '12/1/2016',
+        'Zip_Code': '98101'
     }
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         html, encoding = load_inspection_page()
@@ -87,8 +159,10 @@ if __name__ == '__main__':
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listings(doc)
-    for listing in listings:  # <- add this stuff here.
-        metadata_rows = listing.find('tbody').find_all(
-            has_two_tds, recursive=False
-        )
-        print(len(metadata_rows))
+    for listing in listings:
+        metadata = extract_restaurant_metadata(listing)
+        inspection_row = listing.find_all(is_inspection_row)
+        score_data = extract_score_data(listing)
+        metadata.update(score_data)
+        for key, value in metadata.items():
+            print(key, value)
